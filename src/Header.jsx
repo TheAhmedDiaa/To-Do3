27130fd3fae421cb96e0553a658d4c0b6c157b1a
AhmedDiaa69 <diaaa625@gmail.com>
2025-09-17 04:00:49 +0300

    Fixed the Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ import ThemeToggle from "./ThemeToggle";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 max-h-16 flex justify-between items-center p-4">
+    <header className="fixed top-0 left-0 right-0 max-h-16 flex z-100 justify-between items-center p-4 backdrop-blur-3xl">
       <h1 className="text-2xl font-bold">To-Do List</h1>
       <ThemeToggle />
     </header>
