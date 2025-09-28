b435c0a696f09194ab515bf37c26b98856b7813e
AhmedDiaa69 <diaaa625@gmail.com>
2025-09-29 02:35:53 +0300

    Add local storage functionality to task management context

--- src/TasksContext.jsx
+++ src/TasksContext.jsx
@@ -1,10 +1,22 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const TasksContext  = createContext();
 
+
 export default function TasksProvider({children}) {
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   return(
     <TasksContext.Provider value={{ tasks, setTasks }}>
